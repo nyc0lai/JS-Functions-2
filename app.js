a5c0f699806a51398c3259ea98d274cb9774f930
nyc0lai <nyc0lai@gmail.com>
2021-03-24 23:06:46 +0200

    finisare proect

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
+const userInput = prompt("Enter a choice Rock, Paper or Scissors:");
 
 const getUserChoice = userInput => {
+
     userInput = userInput.toLowerCase();
+    
     if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors') {
         return userInput;
      } else { 
-        console.log('Error!');
+        console.log('Choose only between Rock, Paper or Scissors');
      }
 };
 
@@ -28,13 +31,46 @@ function getComputerChoice() {
 
 function determineWinner (userChoice, computerChoice){
     if (userChoice === computerChoice) {
-        return 'game was a tie';
-    }
+        console.log(`Both players selected ${userChoice}. It's a tie!`);
+    } else {
 
-    if (userChoice === 'rock') {
+            if (userChoice === 'rock') {
 
-    }
+                if (computerChoice === 'paper') {
+
+                console.log('Paper covers rock! Computer win!');
+
+                } else {console.log('Rock smashes scissors! You win!');}
+            } 
+
+            if (userChoice ==='paper') {
+
+                if (computerChoice === 'scissors') {
+
+                console.log('Scissors cuts paper! Computer win!');
+            
+                } else {console.log('Paper covers rock! You win!');}
+            } 
+
+            if (userChoice === 'scissors') {
+
+                if (computerChoice === 'rock') {
+
+                console.log('Rock smashes scissors! Computer winn!');
+            
+                } else {console.log('Scissors cuts paper! You win!}');}
+            } 
+            }
+ }
+
+function playGame () {
+    userChoice = getUserChoice(userInput);
+    computerChoice = getComputerChoice();
+    determineWinner (userChoice, computerChoice);
+    
 }
 
+playGame();
 
-
+console.log(`You\`re shose: ${userInput}`);
+console.log(`Computer shose: ${computerChoice}`);
